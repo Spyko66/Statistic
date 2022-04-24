a20e55e1ad775af9f273ba81f05c9e53a6b309bf
OscarS <sotelo.m.oscar@gmail.com>
2022-04-23 22:34:01 -0500

    Add files via upload

--- showMenu.cpp
+++ showMenu.cpp
@@ -9,7 +9,7 @@ int showMenu(){
     int selectedOption;
 
     cout<<"What do you want to do?\n\n";
-    cout<<"1)  Show dominium, range and the processed data"<<endl<<"2) Show medidas de tendencia central(mediana, media y moda)"<<endl<<"3) All the previous ones\n";
+    cout<<"1)  Show dominium, range and the processed data"<<endl<<"2) Show measures of central tendency (mean, median and mode)"<<endl<<"3) Both\n";
     cin>>selectedOption;
     
     return selectedOption;
